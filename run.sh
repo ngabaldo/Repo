2c16f94c24d94272a069f094ad1086cf23189bb0
ngabaldo <nidia.gabaldon@gmail.com>
2016-11-10 09:31:49 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name="python"
-#SBATCH --output="python.%j.%N.out"
-#SBATCH --partition=shared
-#SBATCH --share
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --export=ALL
-#SBATCH -t 48:00:00
-module load python
 cd paymo_output
 cp ../src/antifraud.py .
 cp ../paymo_input/batch_payment.txt .
