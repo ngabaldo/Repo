2365ed6605f353a081f4ea3b9650796ce599d7ef
ngabaldo <nidia.gabaldon@gmail.com>
2017-04-03 16:30:44 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-cd paymo_output
-cp ../src/antifraud.py .
-cp ../paymo_input/batch_payment.txt .
-cp ../paymo_input/stream_payment.txt .
-./antifraud.py
-rm batch_payment.txt
-rm stream_payment.txt
-rm antifraud.py
+#Executing the program with the input directory log_input and ouput the fiels in the directory log_output
+python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
