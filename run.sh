7a86526926571ea9be65a4b3c987c4ed9ba0c372
ngabaldo <nidia.gabaldon@gmail.com>
2017-04-05 18:05:54 -0600

    Update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Executing the program with the input directory log_input and ouput the files in the directory log_output
-python ./src/src.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt ./log_output/errors.txt
+python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt ./log_output/errors.txt
